4870d416ed68ddd673f98ffb29d51d7e54e66ce0
Yessin kouki <94795583+koukiyessine@users.noreply.github.com>
2024-11-18 12:44:04 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false ,
   apiUrl: 'http://193.95.57.13:8082/SpringMVC'
 };
 
